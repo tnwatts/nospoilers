bb9a769d3edbed24a74ee5e776f172d10b5703c4
sashankrayapudi <sashank.rayapudi@gmail.com>
2022-08-18 11:28:22 -0700

    reconfig deployment

--- nospoilers/settings.py
+++ nospoilers/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from pathlib import Path
+
+import environ
+
+environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,5 +133,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-
-django_on_heroku.settings(locals())
